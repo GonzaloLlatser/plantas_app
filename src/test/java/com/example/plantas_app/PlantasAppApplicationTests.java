778fd3fda1ca3e7a7c56817e85fcb2754ad00f6d
Gonzalo Llatser <gonllat@gmail.com>
2025-03-06 10:34:45 +0100

    Initial commit

---
+++ src/test/java/com/example/plantas_app/PlantasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plantas_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
